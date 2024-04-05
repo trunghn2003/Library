c322f824ff56c635e5b4461b34d89021f19029c4
Trung <hvthn126@gmail.com>
2024-04-05 08:20:24 +0700

    init Genre model

---
+++ LibraryTTCS/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libraby.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID { get; set; }
+        public string GenreName { get; set; }
+    }
+}
