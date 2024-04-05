d561bd5360264d6e7c3fd4647c2c7b0d52e852b1
Trung <hvthn126@gmail.com>
2024-04-05 08:19:08 +0700

    init Author model

---
+++ LibraryTTCS/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libraby.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
