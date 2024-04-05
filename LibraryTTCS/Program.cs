d9cbd716fc425b4f2a3b7564a4530fa8e05d3c65
Trung <hvthn126@gmail.com>
2024-04-05 08:25:22 +0700

    config dbcontext

---
+++ LibraryTTCS/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LibraryTTCS.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<LibraryTTCSContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryTTCSContext") ?? throw new InvalidOperationException("Connection string 'LibraryTTCSContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
